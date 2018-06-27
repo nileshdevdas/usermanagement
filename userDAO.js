68add5c9d59e00f5646f7cf3f8984e138e68eb74
DESKTOP-1L38NVA\Nilesh Devdas <nilesh.devdas@gmail.com>
2018-06-27 12:25:27 -0400

    New adds

--- userDAO.js
+++ userDAO.js
@@ -1,12 +1,25 @@
 module.exports = {
   create : function(object , cb){
+    cb(null, object);
   } ,
   find : function(object, cb){
+    var user = {};
+    user.username = "nilesh";
+    user.password = "password";
+    cb(null, user);
   },
   update : function(object , cb){
+    cb(null,object);
   },
   delete : function(object , cb){
+    var user = {};
+    user.message = "success";
+    cb(null, user);
   },
   findall : function(cb){
+    var user = {};
+    user.username = "nilesh";
+    user.password = "password";
+    cb(null, user);
   }
 }
--- userService.js
+++ userService.js
@@ -1,11 +1,41 @@
 var express = require("express");
 var app = express();
 var userDAO = require("./userDAO.js");
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+http://localhost:5000/hello ---->
+app.get("/hello" , function(request, response){
+    // get the request data
+    console.log(request.params)
 
-app.get("/user/:userid", function(request, response){
-    var user =  // fetch the i from the request
-      userDAO.find(userid, function(err, result){
-        response.setHeader("content-type", "application/json");
+    // write the business logic 
+
+    // respond to user
+    response.end("return to user");
+});
+
+app.get("/user/:userid/:username", function(request, response){
+  var userid = request.params.userid;
+  var username = request.params.username;
+  userDAO.find(userid, function(err, result){
+    console.log("got result")
+    response.setHeader("content-type", "application/json");
+    if(err){
+      console.log("No Such User ");
+      var error = {};
+      error.errorCode = 1;
+      error.errorMessage = "Invalid User";
+      response.end(JSON.stringify(error));
+    }else{
+      response.end(JSON.stringify(result));
+    }
+  });
+});
+
+app.post("/user", jsonParser, function(request, response){
+    var user = request.body;
+    userDAO.create(user, function(err, result){
+          response.setHeader("content-type", "application/json");
           if(err){
             console.log("No Such User ");
             var error = {};
@@ -13,26 +43,41 @@ app.get("/user/:userid", function(request, response){
             error.errorMessage = "Invalid User";
             response.end(JSON.stringify(error));
           }else{
-            response.write(JSON.stringify(result));
+            response.end(JSON.stringify(result));
           }
     });
-
-    app.post("/user", function(request, response){
-        var user =  bodyParser.parse(request);
-          userDAO.save(user, function(err, result){
-            response.setHeader("content-type", "application/json");
-              if(err){
-                console.log("No Such User ");
-                var error = {};
-                error.errorCode = 1;
-                error.errorMessage = "Invalid User";
-                response.end(JSON.stringify(error));
-              }else{
-                response.write(JSON.stringify(result));
-              }
-        });
-  // fetch userid // call the dao ?
 });
 
+app.put("/user", jsonParser, function(request, response){
+    var user = request.body;
+    userDAO.create(user, function(err, result){
+          response.setHeader("content-type", "application/json");
+          if(err){
+            console.log("No Such User ");
+            var error = {};
+            error.errorCode = 1;
+            error.errorMessage = "Invalid User";
+            response.end(JSON.stringify(error));
+          }else{
+            response.end(JSON.stringify(result));
+          }
+    });
+});
+
+app.post("/user", jsonParser, function(request, response){
+    var user = request.body;
+    userDAO.create(user, function(err, result){
+          response.setHeader("content-type", "application/json");
+          if(err){
+            console.log("No Such User ");
+            var error = {};
+            error.errorCode = 1;
+            error.errorMessage = "Invalid User";
+            response.end(JSON.stringify(error));
+          }else{
+            response.end(JSON.stringify(result));
+          }
+    });
+});
 
 app.listen(5000, function(){});
